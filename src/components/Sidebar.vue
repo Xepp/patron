c2bd1c4243fdc1b27237ee456c04bf78a50fad49
Sina Khorami <sina_khorami@comp.iust.ac.ir>
2020-04-28 00:05:12 +0430

    feat: added sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="panel-sidebar"
+  >
+    <router-link
+      class="sidebar-logo"
+      :to="{ name: 'home' }"
+    >
+      <b-img
+        src="@/assets/logo.png"
+        alt="Patron"
+      />
+    </router-link>
+    <router-link
+      class="nav-item"
+      :to="{ name: 'sentiment' }"
+    >
+      <b-icon-list />
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
+import './sass/main.scss'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 
 Vue.config.productionTip = false
 
