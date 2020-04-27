ff4da9d3f0109f99142f29b4d7846d1acf416574
Sina Khorami <sina_khorami@comp.iust.ac.ir>
2020-04-27 18:06:47 +0430

    feat: added basic of service and sentiment api routes

---
+++ src/service/routes.js
@@ -0,0 +1,12 @@
+export default {
+
+  // Sentiment
+  getRandomSentiment: {
+    method: 'GET',
+    url: '/api/sentiment/random'
+  },
+  updateSentiment: {
+    method: 'PATCH',
+    url: '/api/sentiment/'
+  }
+}
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: process.env.VUE_APP_API_SERVER
+      }
+    }
+  }
 }
