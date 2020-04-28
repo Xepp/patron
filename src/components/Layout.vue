963c0a9254a775338c547f112c3ea86d83ae8687
Sina Khorami <sina_khorami@comp.iust.ac.ir>
2020-04-28 11:23:47 +0430

    feat: added layout

---
+++ src/components/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="panel-layout"
+  >
+    <Sidebar />
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar'
+
+export default {
+  name: 'Layout',
+  components: {
+    Sidebar
+  }
+}
+</script>
